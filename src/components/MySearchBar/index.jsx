791f9c7e7a72b7a2974bc3d39d4c98504ebd9300
Huangmiaoz <1873151053@qq.com>
2023-06-06 13:18:43 +0800

    search bar

--- src/components/MySearchBar/index.jsx
+++ src/components/MySearchBar/index.jsx
@@ -1,22 +1,45 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import {Input} from 'antd-mobile'
-import { LocationOutline,DownOutline} from 'antd-mobile-icons'
+import { useLocation, useNavigate } from 'react-router-dom';
+import {Input, NavBar} from 'antd-mobile'
+import { LocationOutline,DownOutline } from 'antd-mobile-icons'
 import PropTypes from 'prop-types'
 const MySearchBar = ({city}) => {
     const history = useNavigate(); 
-    return (
-        <div style={{width:'100%',height:'30px',display:'flex',justifyContent: 'space-between'}}>
-            <span style={{marginLeft:'10px',marginTop:'5px',fontSize:'16px'}} onClick={() => history('/citylist')}>{city}<DownOutline /></span>
-            
+    const location = useLocation();
+
+    const pathname = location.pathname;
+    const arrow_show = pathname === '/home' ? true : false;
+
+    const right = (
+        <LocationOutline 
+            // style={{marginRight:'18px',marginTop:'5px'}} 
+            fontSize={20} 
+            onClick={() => history('/mappage')} 
+        />
+      )
+
+    const left = (
+        <span 
+            // style={{marginLeft:'10px',margin:'10px',fontSize:'16px'}} 
+            onClick={() => history('/citylist')}
+        >
+            {city}
+            <DownOutline />
+        </span>
+      )    
+    
+    const middle = (
             <div
                 onClick={() => history('/home/search')}
-                style={{backgroundColor:'#fff'}}
             >
-               
-               <Input placeholder='请输入小区或地址' disabled/>
+                <Input placeholder='请输入小区或地址' disabled/>
             </div>
-            <LocationOutline style={{marginRight:'18px',marginTop:'5px'}} fontSize={20} onClick={() => history('/mappage')} />
+        )
+    return (       
+        <div>
+            <NavBar right={right} left={arrow_show ? left : null} backArrow={null} onBack={() => { navigate(-1) }}>
+                {middle}
+            </NavBar>
         </div>
     )
 }
